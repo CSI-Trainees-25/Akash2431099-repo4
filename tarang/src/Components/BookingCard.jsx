cbff9545c2ae7829c36fcbfa3cddeb6b8222c9fa
Akash Kushwaha <23kushwahaakash@gmail.com>
2025-10-29 21:50:14 +0530

    Updated HeroSection.jsx

---
+++ tarang/src/Components/BookingCard.jsx
@@ -0,0 +1,23 @@
+
+// function BookingCard() {
+//   return (
+//     <section className='relative w-full flex justify-center -mt-12 z-20'>
+//         <div className="bg-white w-11/12 rounded-2xl shadow-lg border border-gray-100 overflow-hidden">
+//             <div className="flex justify-around md:justify-start gap-4 px-6 py-4 border-b border-gray-200 bg-white rounded-t-2xl">
+//                 {[
+//                     { icon: Plane, label: "Flights" },
+//                     { icon: Hotel, label: "Hotels" },
+//                     { icon: Bus, label: "Buses" },
+//                     { icon: Train, label: "Trains" },
+//                     { icon: Car, label: "Cabs" },
+//                 ].map(({ icon: Icon, label }) => (
+//                 <button key={label} className="flex items-center gap-2 px-3 py-1.5 rounded-full text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 transition"><Icon size={16} />{label}</button>
+//                 ))}
+//             </div>
+//         </div>
+      
+//     </section>
+//   )
+// }
+
+// export default BookingCard
