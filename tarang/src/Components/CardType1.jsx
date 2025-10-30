fdb6a97c088b17ca348bddf559cb8209907f4bbd
Akash Kushwaha <23kushwahaakash@gmail.com>
2025-10-30 21:47:38 +0530

    Added destination cards

--- tarang/src/Components/CardType1.jsx
+++ tarang/src/Components/CardType1.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
-function CardType1() {
+function CardType1({image,state,city,tagline="Discover for yourself"}) {
   return (
-    <div>
-      
+    <div className="overflow-hidden shadow-md hover:shadow-lg transition w-155 flex flex-col justify-center items-center bg-white">
+      <img src={image} alt={city} className="w-full h-45 object-cover" />
+      <div className="p-5 text-center">
+        <p className="text-xs uppercase text-gray-500 tracking-widest">{state}</p>
+        <h1 className="text-3xl font-semibold text-gray-800">{city}</h1>
+        <p className="text-gray-800 mt-3">{tagline}</p>
+      </div>
     </div>
-  )
+  );
 }
-
 export default CardType1
--- tarang/src/Pages/Home.jsx
+++ tarang/src/Pages/Home.jsx
@@ -3,15 +3,45 @@ import Header from '../Components/Header'
 import HeroSection from '../Components/HeroSection'
 import BookingCard from '../Components/BookingCard'
 import CardType1 from '../Components/CardType1'
+import landour from '../Images/landour.png'
+import mahaKumbh from '../Images/mahaKumbh.png'
+import vaishnoDevi from '../Images/vaishnoDevi.png'
+import vrindavan from '../Images/vrindavan.png'
 
 function Home() {
   return (
-    <div>
+    <div className='bg-gray-50 '>
       <Header/>
       <HeroSection/>
       <BookingCard/>
-      <CardType1/>
-      <h1>hey</h1>
+      <div className='mt-10 flex justify-center'>
+        <h1 className='text-2xl pt-10 pb-20'>Featured destinations from</h1>
+        <h1 className='font-bold text-2xl py-10 ml-2'>India</h1>
+      </div>
+      <div className='w-12/12 flex flex-col gap-30 items-center'>
+        <div className='flex gap-30'>
+          <CardType1
+          image={landour}
+          state="Uttrakhand"
+          city="Landour"/>
+
+          <CardType1
+          image={vaishnoDevi}
+          state="Jammu & Kashmir"
+          city="Shree Mata Vaishno Devi"/>
+        </div>
+        <div className='flex gap-30'>
+          <CardType1
+          image={mahaKumbh}
+          state="Uttar Pradesh"
+          city="Maha Kumbh"/>
+
+          <CardType1
+          image={vrindavan}
+          state="Uttar Pradesh"
+          city="Vrindavan"/>
+        </div>
+      </div>
     </div>
   )
 }
